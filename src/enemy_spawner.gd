57efdf8088ea366810eeb373b2b9f0ac99f6916f
minkaizen <martin.g.cao@gmail.com>
2025-07-02 10:59:04 +1000

    feat: randomize time between enemy spawns

--- src/enemy_spawner.gd
+++ src/enemy_spawner.gd
@@ -1,15 +1,19 @@
 extends Node2D
 
 const ROCK_SCENE = preload('res://src/rock.tscn')
+const MAX_WAIT_TIME = 2
+const MIN_WAIT_TIME = 0.2
 
 @onready var timer = %Timer
 
+
 func _ready() -> void:
+	timer.wait_time = 0.1
 	timer.connect('timeout', on_timer_timeout)
 
 func on_timer_timeout() -> void:
-	print('spawner timeout')
 	var rock = ROCK_SCENE.instantiate()
 	rock.position = self.global_position
-	get_tree().current_scene.add_child(rock)
+	get_parent().add_child(rock)
+	timer.wait_time = randf_range(MIN_WAIT_TIME, MAX_WAIT_TIME)
 
