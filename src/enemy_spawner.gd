1fcadde65ebf3d316c277be71c56e3a3651a2281
minkaizen <martin.g.cao@gmail.com>
2025-07-02 12:47:52 +1000

    feat: spawn cactus and rocks randomly

--- src/enemy_spawner.gd
+++ src/enemy_spawner.gd
@@ -1,19 +1,24 @@
 extends Node2D
 
 const ROCK_SCENE = preload('res://src/rock.tscn')
+const CACTUS_SCENE = preload('res://src/cactus.tscn')
 const MAX_WAIT_TIME = 2
-const MIN_WAIT_TIME = 0.2
+const MIN_WAIT_TIME = 0.5
+const ENEMIES = [
+	ROCK_SCENE,
+	CACTUS_SCENE,
+]
 
 @onready var timer = %Timer
 
 
 func _ready() -> void:
-	timer.wait_time = 0.1
-	timer.connect('timeout', on_timer_timeout)
+	spawn_enemy()
+	timer.connect('timeout', spawn_enemy)
 
-func on_timer_timeout() -> void:
-	var rock = ROCK_SCENE.instantiate()
-	rock.position = self.global_position
-	get_parent().add_child(rock)
+func spawn_enemy() -> void:
+	var enemy = ENEMIES[randi_range(0, ENEMIES.size() - 1)].instantiate()
+	enemy.position = self.global_position
+	get_parent().add_child(enemy)
 	timer.wait_time = randf_range(MIN_WAIT_TIME, MAX_WAIT_TIME)
-
+	timer.start()
