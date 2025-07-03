c482e4513eef704b9e461e11c9c79b3e1799285a
minkaizen <martin.g.cao@gmail.com>
2025-07-03 10:25:23 +1000

    feat: add score

---
+++ src/score.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var score = 0.0:
+	set(value):
+		score = value
+		var score_rounded = roundi(score)
+		score_text = "Score: %010d" % score_rounded
+var score_text = "Score: %010d" % 0
+var speed = 10
+
+@onready var label = %Label
+
+func reset() -> void:
+	score = 0
+
+func _physics_process(delta: float) -> void:
+	score += delta * speed
+	print(score)
+	label.text = score_text
+
+
